95b9dc02546c01366a1e4ccb781b849cf478557a
staff2 <staff2@localhost.localdomain>
2019-04-24 19:11:52 +0530

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { WelcomePageComponent } from './pages/welcome-page/welcome-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+{path:'', component:WelcomePageComponent}
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WelcomePageComponent } from './pages/welcome-page/welcome-page.component';
+import { WelcomeNavComponent } from './components/welcome-nav/welcome-nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomePageComponent,
+    WelcomeNavComponent
   ],
   imports: [
     BrowserModule,
