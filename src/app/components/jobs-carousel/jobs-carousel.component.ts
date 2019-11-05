6a33de85fdb8fb01952ff4b272fc01550e8acb1e
sai1004 <saikiran.bavandla@gmail.com>
2019-11-05 19:53:48 +0530

    commit

--- src/app/components/jobs-carousel/jobs-carousel.component.ts
+++ src/app/components/jobs-carousel/jobs-carousel.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input, Output, EventEmitter } from "@angular/core";
 import { Heros } from "../../entities/heros";
 import { HeroService } from '../welcome-facts/hero.service';
-
+declare var $: any;
 @Component({
   selector: "app-jobs-carousel",
   templateUrl: "./jobs-carousel.component.html",
@@ -11,18 +11,10 @@ export class JobsCarouselComponent {
 
 
   constructor( private heroService: HeroService) {
-this.heroService = heroService;
+ 
   }
 
-  dummy;
+  
 
-
-
-  onClick() {
-    this.dummy = " hello there!!";
-  }
-
-  delete() {
-    this.dummy = this.delete();
-  }
+ 
 }
